2dd0d2c8adc70a241b471b5d3b50c45cd0e10b05
Paul <paulchien_5@hotmail.com>
2022-03-14 21:56:14 +0800

    proper miumiu

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-let ans=prompt("What is your name?", "name")
-let reply = document.createElement("p");
+let ans=prompt("What is your name?", "name");
 if (ans == "miumiu" | ans == "miu miu")
-    {
-        reply.innerHTML="HELLO MIUMIU I MISS U VERY MUCH <3"
-    }
-else{
-    reply.innerHTML="Actually I don't care"
-    }
-document.getElementById("check_for_miumiu").appendChild(reply);
+{
+    let tag = document.getElementById("miumiu")
+    let text = document.createTextNode("HELLO MIUMIU I MISS U VERY MUCH <3");
+    tag.appendChild(text);
+}
+else
+{
+    let tag = document.getElementById("miumiu")
+    let text = document.createTextNode("Actually I don't care hehehehehe");
+    tag.appendChild(text);
+}
